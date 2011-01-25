18d5eabab958ef3f7ced9ff263afdd51c30aa727
amphiprions@gmail.com <amphiprions@gmail.com@bd4a1646-4166-b512-6b31-09a00a1dab00>
2011-01-25 23:50:46 +0000

    Fix search

--- trunk/TricTrac/src/org/amphiprion/trictrac/CollectionActivityGroup.java
+++ trunk/TricTrac/src/org/amphiprion/trictrac/CollectionActivityGroup.java
@@ -23,10 +23,15 @@ import org.amphiprion.trictrac.entity.Collection;
 import org.amphiprion.trictrac.entity.Search;
 
 import android.app.ActivityGroup;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * @author amphiprion
@@ -40,6 +45,9 @@ public class CollectionActivityGroup extends ActivityGroup {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		instance = this;
+		CollectionList.group = this;
+		GameList.group = this;
+
 		// Start the root activity withing the group and get its view
 		View view = getLocalActivityManager().startActivity("CollectionList",
 				new Intent(this, CollectionList.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)).getDecorView();
@@ -47,6 +55,42 @@ public class CollectionActivityGroup extends ActivityGroup {
 		// Replace the view of this ActivityGroup
 		setContentView(view);
 		isCollectionDisplayed = true;
+	}
+
+	@Override
+	public boolean onSearchRequested() {
+		if (!isCollectionDisplayed) {
+			// startSearch(GameList.instance.getQuery(), true, null, false);
+			AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+			// alert.setTitle(R.string.search_hint);
+			alert.setMessage(R.string.search_hint);
+
+			// Set an EditText view to get user input
+			final EditText input = new EditText(this);
+			input.setTag(GameList.instance.getQuery());
+			alert.setView(input);
+
+			alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+					String value = "" + input.getText();
+					value = value.replaceAll(" ", "%");
+					GameList.instance.setQuery(value);
+				}
+			});
+
+			alert.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+					// Canceled.
+				}
+			});
+
+			alert.show();
+
+			return true;
+		} else {
+			return true;
+		}
 	}
 
 	public void showCollectionList() {
@@ -79,5 +123,61 @@ public class CollectionActivityGroup extends ActivityGroup {
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		menu.clear();
+		if (isCollectionDisplayed) {
+			MenuItem addAccount = menu.add(0, ApplicationConstants.MENU_ID_ADD_COLLECTION, 0, R.string.add_collection);
+			addAccount.setIcon(android.R.drawable.ic_menu_add);
+
+			MenuItem searchTrictrac = menu.add(0, ApplicationConstants.MENU_ID_SEARCH_TRICTRAC_GAME, 1,
+					R.string.menu_search_trictrac);
+			searchTrictrac.setIcon(android.R.drawable.ic_menu_search);
+
+			MenuItem account = menu.add(1, ApplicationConstants.MENU_ID_ACCOUNT, 2, R.string.trictrac_account);
+			account.setIcon(android.R.drawable.ic_menu_info_details);
+
+			MenuItem preference = menu.add(2, ApplicationConstants.MENU_ID_PREFERENCE, 3, R.string.preference);
+			preference.setIcon(android.R.drawable.ic_menu_preferences);
+		} else {
+			if (GameList.instance.getSearch() != null || GameList.instance.getQuery() != null) {
+				MenuItem clearSearch = menu.add(0, ApplicationConstants.MENU_ID_CLEAR_SEARCH, 0, R.string.clear_filter);
+				clearSearch.setIcon(R.drawable.search_cleared);
+			}
+			MenuItem addAccount = menu.add(0, ApplicationConstants.MENU_ID_CHOOSE_EXISTING_SEARCH, 1,
+					R.string.apply_existing_filter);
+			addAccount.setIcon(R.drawable.search);
+		}
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (isCollectionDisplayed) {
+			if (item.getItemId() == ApplicationConstants.MENU_ID_ADD_COLLECTION) {
+				Intent i = new Intent(this, EditCollection.class);
+				// i.putExtra("COLLECTION", collection);
+				startActivityForResult(i, ApplicationConstants.ACTIVITY_RETURN_CREATE_COLLECTION);
+			} else if (item.getItemId() == ApplicationConstants.MENU_ID_SEARCH_TRICTRAC_GAME) {
+				Intent i = new Intent(this, TricTracGameList.class);
+				startActivityForResult(i, ApplicationConstants.ACTIVITY_RETURN_SEARCH_TRICTRAC_GAME);
+			} else if (item.getItemId() == ApplicationConstants.MENU_ID_PREFERENCE) {
+				Home.openPreference(this);
+			} else if (item.getItemId() == ApplicationConstants.MENU_ID_ACCOUNT) {
+				Home.openAccount(this);
+			}
+		} else {
+			return GameList.instance.onOptionsItemSelected(item);
+		}
+		return true;
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (isCollectionDisplayed) {
+			CollectionList.instance.onActivityResult(this, requestCode, resultCode, data);
+		}
 	}
 }
